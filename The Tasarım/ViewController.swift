6d7e4abe354f400210ecacfe33efcc04d7a91a00
FarukAs <omeraso1998@gmail.com>
2023-01-25 20:52:06 +0300

    Initial Commit

---
+++ The Tasarım/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  The Tasarım
+//
+//  Created by Şeyda Soylu on 25.01.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
